2601960365edfcf64a489705d57562e0a7ac146e
greif.matthias <greif.matthias@outlook.com>
2018-11-07 20:37:36 +0100

    Platform updates

---
+++ app/src/main/java/be/greifmatthias/toddler/Activities/SpeakingExercise.java
@@ -0,0 +1,11 @@
+package be.greifmatthias.toddler.Activities;
+
+import be.greifmatthias.toddler.Exercises.Exercise;
+
+public class SpeakingExercise extends Exercise {
+    public SpeakingExercise(String word) {
+        super(word);
+    }
+
+
+}
