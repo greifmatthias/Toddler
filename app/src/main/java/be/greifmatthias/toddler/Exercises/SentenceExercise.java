daff4e64d08c1a565347fa4e45bcfa6531fe94fc
greif.matthias <greif.matthias@outlook.com>
2018-12-29 15:39:57 +0100

    SentenceExercise Voice added

--- app/src/main/java/be/greifmatthias/toddler/Exercises/SentenceExercise.java
+++ app/src/main/java/be/greifmatthias/toddler/Exercises/SentenceExercise.java
@@ -35,17 +35,21 @@ public class SentenceExercise extends Exercise {
 
     private class Sentence{
         private String _sentence;
+        private int _voice;
         private boolean _valid;
 
-        public Sentence(String sentence, boolean valid){
+        public Sentence(String sentence, int voice, boolean valid){
             this._sentence = sentence;
+            this._voice = voice;
             this._valid = valid;
         }
 
         public String getSentence(){
             return this._sentence;
         }
-
+        public int getVoice(){
+            return this._voice;
+        }
         public boolean getValid(){
             return this._valid;
         }
@@ -60,64 +64,64 @@ public class SentenceExercise extends Exercise {
         switch (this._word){
             case "De duikbril":
                 if(part == 0){
-                    return new Sentence("Met zijn duikbril kan de jongen de vissen onder water goed bekijken.", true);
+                    return new Sentence("Met zijn duikbril kan de jongen de vissen onder water goed bekijken.", R.raw.sentence_duikbril_true, true);
                 }
 
-                return new Sentence("Met een duikbril kan ik schrijven op papier.", false);
+                return new Sentence("Met een duikbril kan ik schrijven op papier.", R.raw.sentence_duikbril_false, false);
             case "Het klimtouw":
                 if(part == 0){
-                    return new Sentence("In de turnzaal klim ik omhoog in het klimtouw.", true);
+                    return new Sentence("In de turnzaal klim ik omhoog in het klimtouw.", R.raw.sentence_klimtouw_true, true);
                 }
 
-                return new Sentence("Ik wacht op de bus in het klimtouw.", false);
+                return new Sentence("Ik wacht op de bus in het klimtouw.", R.raw.sentence_klimtouw_false, false);
             case "Het kroos":
                 if(part == 0){
-                    return new Sentence("De vijver is groen door het kroos.", true);
+                    return new Sentence("De vijver is groen door het kroos.", R.raw.sentence_kroos_true, true);
                 }
 
-                return new Sentence("Oma en het kroos zitten in de auto.", false);
+                return new Sentence("Oma en het kroos zitten in de auto.", R.raw.sentence_kroos_false, false);
             case "Het riet":
                 if(part == 0){
-                    return new Sentence("De eenden zitten bij het water tussen het riet.", true);
+                    return new Sentence("De eenden zitten bij het water tussen het riet.", R.raw.sentence_riet_true, true);
                 }
 
-                return new Sentence("Ik ga naar buiten met mijn jas en het riet aan.", false);
+                return new Sentence("Ik ga naar buiten met mijn jas en het riet aan.", R.raw.sentence_riet_false, false);
             case "De val":
                 if(part == 0){
-                    return new Sentence("Wat was dat een pijnlijke val!", true);
+                    return new Sentence("Wat was dat een pijnlijke val!", R.raw.sentence_val_true, true);
                 }
 
-                return new Sentence("Jan zit op de val aan tafel.", false);
+                return new Sentence("Jan zit op de val aan tafel.", R.raw.sentence_val_false, false);
             case "Het kompas":
                 if(part == 0){
-                    return new Sentence("Omdat papa niet weet waar we naartoe moeten lopen, kijkt hij op zijn kompas.", true);
+                    return new Sentence("Omdat papa niet weet waar we naartoe moeten lopen, kijkt hij op zijn kompas.", R.raw.sentence_kompas_true, true);
                 }
 
-                return new Sentence("Mama belt met het kompas naar papa.", false);
+                return new Sentence("Mama belt met het kompas naar papa.", R.raw.sentence_kompas_false, false);
             case "Steil":
                 if(part == 0){
-                    return new Sentence("Jan loopt de steile berg omhoog.", true);
+                    return new Sentence("Jan loopt de steile berg omhoog.", R.raw.sentence_steil_true, true);
                 }
 
-                return new Sentence("Papa leest een steil verhaaltje voor.", false);
+                return new Sentence("Papa leest een steil verhaaltje voor.", R.raw.sentence_steil_false, false);
             case "De zwaan":
                 if(part == 0){
-                    return new Sentence("In de vijver in het park zwemt een witte zwaan.", true);
+                    return new Sentence("In de vijver in het park zwemt een witte zwaan.", R.raw.sentence_zwaan_true, true);
                 }
 
-                return new Sentence("De zwaan fietst in het park.", false);
+                return new Sentence("De zwaan fietst in het park.", R.raw.sentence_zwaan_false, false);
             case "Het kamp":
                 if(part == 0){
-                    return new Sentence("De kinderen zitten te eten tussen de tenten van het kamp.", true);
+                    return new Sentence("De kinderen zitten te eten tussen de tenten van het kamp.", R.raw.sentence_kamp_true, true);
                 }
 
-                return new Sentence("Jonas wast zich met het kamp.", false);
+                return new Sentence("Jonas wast zich met het kamp.", R.raw.sentence_kamp_false, false);
             case "De zaklamp":
                 if(part == 0){
-                    return new Sentence("De jongen schijnt met de zaklamp in de donkere grot.", true);
+                    return new Sentence("De jongen schijnt met de zaklamp in de donkere grot.", R.raw.sentence_zaklamp_true, true);
                 }
 
-                return new Sentence("Jef opent de deur met de zaklamp.", false);
+                return new Sentence("Jef opent de deur met de zaklamp.", R.raw.sentence_zaklamp_false, false);
             default:
                 return null;
         }
@@ -155,6 +159,7 @@ public class SentenceExercise extends Exercise {
 
             this._current = TypeHelper.getRandom(0, 1);
             ((TextView)view.findViewById(R.id.tvSentence)).setText(_exercise.getSentence(this._current).getSentence());
+            this._activity.setKaatje_voice(_exercise.getSentence(this._current).getVoice());
 
             view.findViewById(R.id.rlRate_yes).setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -184,6 +189,7 @@ public class SentenceExercise extends Exercise {
 
                 this._current = 1 - this._current;
                 ((TextView)view.findViewById(R.id.tvSentence)).setText(_exercise.getSentence(this._current).getSentence());
+                this._activity.setKaatje_voice(_exercise.getSentence(this._current).getVoice());
             }
         }
     }
